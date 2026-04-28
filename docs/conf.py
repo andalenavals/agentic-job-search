0444e39b10c8c3f1aae94b8f8d9a601c98dbb6fb
andalenavals <a.navarro.alsina@gmail.com>
2026-04-28 09:25:00 +0200

    Use Read the Docs Sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,6 @@ extensions = []
 templates_path = []
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_title = "agentic-job-search"
 html_baseurl = "https://andalenavals.github.io/agentic-job-search/"
